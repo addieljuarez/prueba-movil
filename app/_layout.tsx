c6eaca5ec6507e0537a307309de484722a184d0f
Addiel Juarez <addieljuarez@gmail.com>
2025-06-19 22:59:58 -0600

    change tabs to index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,12 +16,13 @@ export default function RootLayout() {
     // Async font loading only occurs in development.
     return null;
   }
+  
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" /> */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
