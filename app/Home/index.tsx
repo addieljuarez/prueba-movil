64ac26a86246023f0c27032455e6a44330eaa4d8
Addiel Juarez <addieljuarez@gmail.com>
2025-06-22 22:10:56 -0600

    feature: add input to search

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, TextInput, View } from 'react-native'
 import { getTasks } from '../API'
 import SafeAreaComponent from '../Components/SafeAreaComponent'
 import { Task } from '../Schemas/tasks'
@@ -21,24 +21,55 @@ export default function HomePage(){
         }
         fetchTasks()
     }, [])
-
+    const onChangeSearch = (text: string) => {
+        if (text.length > 0) {
+            const filteredTasks = tasks.data.filter((task: Task) =>
+                task.title.toLowerCase().includes(text.toLowerCase())
+            )
+            setTasks({
+                ...tasks,
+                data: filteredTasks,
+                success: filteredTasks.length > 0,
+                message: filteredTasks.length > 0 ? 'Tareas filtradas correctamente' : 'No se encontraron tareas'
+            })
+        } else {
+            // Si el input está vacío, volvemos a cargar todas las tareas
+            const fetchTasks = async () => {
+                const getAllTasks = await getTasks(limitTask)
+                setTasks(getAllTasks)
+            }
+            fetchTasks()
+        }
+    }
     return (
         <>
             <SafeAreaComponent>
-                {tasks.success ? (
-                    tasks.data.map((task: Task, index) => (
-                        // <Text key={index}>{task.title}</Text>
-                        <View key={index} style={{ padding: 10, borderBottomWidth: 1, borderColor: '#ccc' }}>
-                            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{task.id}</Text>
-                            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{task.title}</Text>
-                            <Text style={{ color: task.completed ? 'green' : 'red' }}>
-                                {task.completed ? 'Completada' : 'Pendiente'}
-                            </Text>
-                        </View>
-                    ))
-                ) : (
-                    <Text>{tasks.message}</Text>
-                )}
+                {/* aqio va el input del buscador */}
+                <View style={{ padding: 10, backgroundColor: '#f8f8f8', borderRadius: 5, marginBottom: 10 }}>
+                    <TextInput
+                        placeholder="Buscar Tareas"
+                        style={{ height: 40, borderColor: '#ccc', borderWidth: 1, paddingHorizontal: 10 }}
+                        onChangeText={onChangeSearch}
+                    />
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Buscar Tareas</Text>
+                </View>
+                <ScrollView style={{ flex: 1, padding: 10 }}>
+                    {tasks.success ? (
+                        tasks.data.map((task: Task, index) => (
+                            // <Text key={index}>{task.title}</Text>
+                            <View key={index} style={{ padding: 10, borderBottomWidth: 1, borderColor: '#ccc' }}>
+                                <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{task.id}</Text>
+                                <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{task.title}</Text>
+                                <Text style={{ color: task.completed ? 'green' : 'red' }}>
+                                    {task.completed ? 'Completada' : 'Pendiente'}
+                                </Text>
+                            </View>
+                        ))
+                    ) : (
+                        <Text>{tasks.message}</Text>
+                    )}
+                </ScrollView>
+                
             </SafeAreaComponent>
         </>
     )
